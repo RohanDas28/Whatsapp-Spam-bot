ba75e8a9b4fc7a8e73fdd9a22f84f5008d6da0ef
Rohan Das <42172455+RohanDas28@users.noreply.github.com>
2021-04-27 01:47:04 +0530

    Updated the wait time ⏰

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyautogui
 import webbrowser
 import time 
 webbrowser.open("web.whatsapp.com")
-time.sleep(10)
+time.sleep(30)
 while True:
     pyautogui.press("R")
     pyautogui.press("enter")
